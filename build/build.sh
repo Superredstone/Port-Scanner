2270654811aeab1fe12c4f6553f371f8c26dbe9c
Superredstone <patrickcanal3@gmail.com>
2021-06-02 15:06:48 +0200

    Added Android support

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,8 @@
 Name=PortScanner
 
 cd ..
-rsrc.exe -arch amd64 -ico assets/icon.ico
-rsrc.exe -arch arm -ico assets/icon.ico
+/home/r3ddy/go/bin/rsrc -arch amd64 -ico assets/icon.ico
+/home/r3ddy/go/bin/rsrc -arch arm -ico assets/icon.ico
 
 env GOOS=windows GOARCH=amd64 go build -o $Name-windows-amd64.exe -ldflags="-H windowsgui"
 echo Windows amd64 builded
@@ -15,7 +15,4 @@ env GOOS=windows GOARCH=arm go build -o $Name-windows-arm.exe -ldflags="-H windo
 echo Windows arm builded
 
 env GOOS=linux GOARCH=amd64 go build -o $Name-linux-amd64 ../main.go
-echo Linux amd64 builded
-
-env GOOS=android GOARCH=arm go build .
-echo Android builded
+echo Linux amd6
---
+++ build/buildAndroid.bat
@@ -0,0 +1,3 @@
+cd ..
+
+gogio -target android -o build/PortScanner-android.apk -icon assets/icon.png -appid com.portscanner.superredstone .
